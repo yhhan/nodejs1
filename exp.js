08b5434f4bb271301bbe432cf15930a6804c49dc
yhhan <hyh123456@gmail.com>
2023-05-27 13:15:28 +0900

    fresh 1

--- exp.js
+++ exp.js
@@ -13,9 +13,11 @@ console.log("Server is starting..");
 console.log("333");
 console.log("444");
 console.log("555");
+console.log("fresh 1");
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
+
 
