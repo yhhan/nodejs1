008a1c232b7ea29fff4122464f0e0a624170b72a
yhhan <hyh123456@gmail.com>
2023-05-27 14:04:18 +0900

    f2

--- exp.js
+++ exp.js
@@ -13,12 +13,9 @@ console.log("Server is starting..");
 console.log("333");
 console.log("444");
 console.log("555");
-<<<<<<< HEAD
 console.log("fresh 1");
-=======
 console.log("666");
 console.log("777");
->>>>>>> 5e7fb87515748cd7bc822ad2fde7f738e3bd160a
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
